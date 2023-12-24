9a1d4f9fbd2662a3acfac69b5ac6aa036b1d1dc4
tabFor <2244530215@qq.com>
2023-12-24 20:05:32 +0800

    OK

--- src/global.js
+++ src/global.js
@@ -3,8 +3,8 @@ import { Dialog } from "element-ui";
 export default {
   items: [
     {
-      massgge: "欢迎",
-      inputask: "AI"
+      massgge: "欢迎使用AI对话",
+      inputask: "请输入您有问题的合同部分或者您的问题"
     }
   ],
   dialogueID: "",
